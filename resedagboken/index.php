dfcfc2693a7bded1daf0c1267595f85a35460940
Karim Ryde <karim.ryde@learnet.se>
2018-03-29 14:25:36 +0200

    Börjar med grundmallen HTML.

--- resedagboken/index.php
+++ resedagboken/index.php
@@ -1,11 +1,51 @@
 <?php
 /**
-* Beskrivning
+* Hemsidan
 *
 * PHP version 5
-* @category   Kategori
+* @category   Webbtjänst
 * @author     Karim Ryde <karye.webb@gmail.com>
 * @license    PHP CC
 * @link
 */
 ?>
+    <!DOCTYPE html>
+    <html lang="sv">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Resedagboken för dom ressugna</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+
+    <body>
+        <div class="kontainer">
+            <header>
+                <h1>Resedagboken</h1>
+                <nav>
+                    <ul>
+                        <li><a class="aktuell" href="#">Logga in</a></li>
+                        <li><a href="#">Skapa konto</a></li>
+                        <li><a href="#">Andras resor</a></li>
+                    </ul>
+                    <form>
+                        <input type="text" name="sok" placeholder="Sök">
+                    </form>
+                </nav>
+            </header>
+            <main>
+                <img src="images/resa.jpg" alt="Exempel på resa">
+                <h2>På besök i ett tempel</h2>
+                <p>Lorem ipsum dolor amet literally flannel coloring book kickstarter 3 wolf moon pok pok. Polaroid iceland drinking vinegar salvia keytar heirloom gentrify health goth hell of adaptogen edison bulb pour-over echo park gluten-free master cleanse. Church-key prism irony authentic pitchfork kinfolk. Portland cardigan single-origin coffee keytar coloring book, listicle bicycle rights kogi. Forage jianbing photo booth everyday carry ugh, readymade bushwick bicycle rights selvage sriracha kinfolk hashtag slow-carb ennui. Direct trade chillwave vexillologist chicharrones gluten-free.</p>
+                <p>Chambray coloring book lumbersexual, artisan craft beer banjo jianbing gastropub hella waistcoat asymmetrical. Vegan food truck gastropub meh truffaut, tilde cred pitchfork cray. Tumeric man bun af fashion axe vinyl seitan. Selvage marfa cardigan helvetica, artisan fanny pack squid green juice.</p>
+            </main>
+            <footer>
+                <h4>Info</h4>
+                <p>...</p>
+                <h4>Kontakt</h4>
+                <p>...</p>
+            </footer>
+        </div>
+    </body>
+
+    </html>

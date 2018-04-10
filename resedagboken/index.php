cf0bd40bd966944e35786beaa2eedceeec732d8e
Karim Ryde <karim.ryde@learnet.se>
2018-04-10 10:25:30 +0200

    Infoga en inloggningsruta som är modal.

--- resedagboken/index.php
+++ resedagboken/index.php
@@ -17,6 +17,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Resedagboken för dom ressugna</title>
         <link rel="stylesheet" href="styles/style.css">
     </head>
@@ -27,7 +28,7 @@
                 <h1>Resedagboken</h1>
                 <nav>
                     <ul>
-                        <li><a class="aktuell" href="index.php">Logga in</a></li>
+                        <li><a href="#myModal" class="trigger-btn" data-toggle="modal">Logga in</a></li>
                         <li><a href="skapa_konto.php">Skapa konto</a></li>
                         <li><a href="#">Andras resor</a></li>
                         <li>
@@ -56,6 +57,43 @@
                 </div>
             </footer>
         </div>
+
+        <!-- Modal HTML -->
+        <div id="myModal" class="modal fade">
+            <div class="modal-dialog modal-login">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div class="avatar">
+                            <img src="images/avatar.png" alt="Avatar">
+                        </div>
+                        <h4 class="modal-title">Medlem</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="min_sida.php" method="post">
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="anamn" placeholder="Användarnamn" required="required">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="losen" placeholder="Lösenord" required="required">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Logga in</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#">Glömt lösenordet?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     </body>
 
     </html>

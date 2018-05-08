8b748d4032ae849c6e36bc5ddd27b7802c1af951
Karim Ryde <karim.ryde@learnet.se>
2018-05-08 14:24:22 +0200

    Json övning klar.

--- owm_json/london.php
+++ owm_json/london.php
@@ -18,8 +18,7 @@
 <body>
     <?php
     // Url till webbtjänsten
-    $url = "http://api.openweathermap.org/data/2.5/weather?q=London&
-    APPID=22ee1d58f7adae08ee71fa7c0bd24481";
+    $url = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=22ee1d58f7adae08ee71fa7c0bd24481&units=metric";
 
     // Hämtat data från webbtjänsten
     $contents = file_get_contents($url);
@@ -28,10 +27,24 @@
     $jsonData = json_decode($contents);
 
     // Plocka ut intressant data ur json-objektet
+    // Plocka ut $koordinater
     $koordinater = $jsonData->coord;
-    print_r($koordinater);
+    //print_r($koordinater);
+    //echo "<br>";
 
+    // Plocka ut vädret
+    $weather = $jsonData->weather;
+    //print_r($weather);
+    //echo "<br>";
+    $weatherDescription = $jsonData->weather[0]->description;
+    //print_r($weatherDescription);
 
+    $temp_min = $jsonData->main->temp_min;
+    $temp_max = $jsonData->main->temp_max;
+
+    echo "<h3>London</h3>";
+    echo "<p>Max temp: $temp_max&deg; C</p>";
+    echo "<p>Min temp: $temp_min&deg; C</p>";
     ?>
 </body>
 </html>

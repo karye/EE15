b3347de76abf4280a86381a6e5532cc38885c737
Karim Ryde <karim.ryde@learnet.se>
2018-05-08 15:48:35 +0200

    Ny likadan övning.

--- trafiklab_json/sl.php
+++ trafiklab_json/sl.php
@@ -1,52 +1,49 @@
 <?php
 /**
-* Hämta data om närmsta hållplatser från trafiklab (SL)
+* Hämta data trafiklab
 *
-* PHP version 7
-* @category   json övning
+* @category   json data övning
 * @author     Karim Ryde <karye.webb@gmail.com>
 * @license    PHP CC
 * @link
-* API-dok     https://www.trafiklab.se/api/sl-narliggande-hallplatser/dokumentation
-* API-nyckel  5a04359da47042b7837f88a5c61908c9
 */
 ?>
-
 <?php
-// Inställningar
-$SVAR = 50;
+$SVAR = 100;
 $RADIUS = 1000;
 
-// Ta emot POST-data och validera
-$lat = filter_input(INPUT_POST, "lat", FILTER_SANITIZE_STRING);
-$lng = filter_input(INPUT_POST, "lng", FILTER_SANITIZE_STRING);
+$lat = $_POST['lat'];
+$lng = $_POST['lng'];
 
 if ($lat && $lng) {
 
     // Adress till tjänsten
     $url = "http://api.sl.se/api2/nearbystops.json?key=5a04359da47042b7837f88a5c61908c9&originCoordLat=$lat&originCoordLong=$lng&maxresults=$SVAR&radius=$RADIUS";
 
-    // Hämta data och avkoda JSON-formatet
+    // Hämtar data och avkodar json
     $contents = file_get_contents($url);
     $stopData = json_decode($contents);
     $stops = $stopData->LocationList->StopLocation;
-    //print_r($stops);
 
     $markers = "[";
 
-    // Plocka ut all hållplatser och spara
+    // Plocka ut alla hållplatser
     foreach ($stops as $stop) {
         $markers .= "['$stop->name', $stop->lat, $stop->lon],\n";
     }
+
+    // Ta bort sista kommat
     $markers = substr($markers, 0, -2) . "]";
 
-    // Skapa Javascript svaret
+    // Skapa Google Maps kartan som Javascript
     echo "function initMap() {
-            var bounds = new google.maps.LatLngBounds();
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 14
-            });\n";
+        var bounds = new google.maps.LatLngBounds();
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14
+        });\n";
+
     echo "var markers = $markers;\n";
+
     echo "for (i = 0; i < markers.length; i++) {
             var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
             bounds.extend(position);
